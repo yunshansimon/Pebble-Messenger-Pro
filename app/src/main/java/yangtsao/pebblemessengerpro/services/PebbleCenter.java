8d1bd480123e2963a1a4f9c545226115ba4b061e
yunshansimon <yunshansimon@gmail.com>
2014-08-21 23:06:22 +0800

    42% progressed

--- app/src/main/java/yangtsao/pebblemessengerpro/services/PebbleCenter.java
+++ app/src/main/java/yangtsao/pebblemessengerpro/services/PebbleCenter.java
@@ -25,6 +25,9 @@ public class PebbleCenter extends Service {
     public final static int PEBBLE_SEND_MESSAGE_TABLE=4;
     public final static int PEBBLE_SEND_CALL_TABLE=5;
 
+    private static final int PREPARE_MESSAGE=1;
+    private static final int PREPARE_CALL=2;
+
     private int fChars;  //line contain chars
     private int fLines;  //page contain lines
     private int fLength; //package max length
@@ -32,14 +35,17 @@ public class PebbleCenter extends Service {
     private Queue<PebbleDictionary> sendQueue;
     private Queue<PebbleMessage> waitQueue;
     private boolean pebbleBusy=false;
+    private boolean endOfPage=false;
     private Time busyBegin;
+    private final static long MAX_WAITING_MILLIS=20000;
 
     private Handler prepareThreadHandler;
+    private Handler sendMsgThreadHandler;
 
     private Messenger rMessageProcessingHandler=null;
 
 
-    private ServiceConnection connToMessagePreocessing=new ServiceConnection() {
+    private ServiceConnection connToMessageProcessing=new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             rMessageProcessingHandler=new Messenger(iBinder);
@@ -57,14 +63,14 @@ public class PebbleCenter extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        bindService(new Intent(this, MessageProcessingService.class), connToMessagePreocessing,
+        bindService(new Intent(this, MessageProcessingService.class), connToMessageProcessing,
                 Context.BIND_AUTO_CREATE);
 
     }
 
     @Override
     public void onDestroy() {
-        unbindService(connToMessagePreocessing);
+        unbindService(connToMessageProcessing);
         super.onDestroy();
     }
 
@@ -106,6 +112,54 @@ public class PebbleCenter extends Service {
 
     class PrepareHandler extends Handler{
         public PrepareHandler (Looper looper){
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
+            switch (msg.what){
+                case PREPARE_MESSAGE:{
+                    PebbleMessage dealPM=waitQueue.poll();
+                    if(dealPM==null){
+                        return;
+                    }
+                    while(pebbleBusy){
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Time nowTime=new Time();
+                        nowTime.setToNow();
+                        if( Math.abs(nowTime.toMillis(true)- busyBegin.toMillis(true))>MAX_WAITING_MILLIS){
+                            sendQueue.clear();
+                            pebbleBusy=false;
+                        }
+                    }
+
+
+                }
+                    break;
+                case PREPARE_CALL:
+                    break;
+            }
+
+
+
+        }
+    }
+
+    class SendMsgThread extends Thread{
+        @Override
+        public void run() {
+            Looper.prepare();
+            sendMsgThreadHandler=new SendMsgHandler(Looper.myLooper());
+            Looper.loop();
+        }
+    }
+    class SendMsgHandler extends Handler{
+        public SendMsgHandler(Looper looper){
             super(looper);
         }
 
