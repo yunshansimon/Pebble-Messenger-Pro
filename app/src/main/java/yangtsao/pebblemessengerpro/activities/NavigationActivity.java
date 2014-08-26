28d4dc71a50e27ed048bc50d3fcc8ddd906f445a
yang cao <yunshansimon@126.com>
2014-08-26 14:56:00 +0800

    Processing 50%. The point is how to design sending queue and block the send queue

--- app/src/main/java/yangtsao/pebblemessengerpro/activities/NavigationActivity.java
+++ app/src/main/java/yangtsao/pebblemessengerpro/activities/NavigationActivity.java
@@ -1,22 +1,17 @@
 package yangtsao.pebblemessengerpro.activities;
 
-import android.app.Activity;
-
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
 import yangtsao.pebblemessengerpro.R;
 
 public class NavigationActivity extends Activity
